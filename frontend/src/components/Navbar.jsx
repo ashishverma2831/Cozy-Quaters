db864a85487b96e01cf3df732593f56e57c6af00
ashishverma2831 <ashishverma510207@gmail.com>
2024-01-19 16:16:26 +0530

    Update Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,48 +1,34 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  const cities = ['Lucknow','Barabanki','Kanpur','Banaras']
   return (
     <>
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
-     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+        <nav className="border-gray-200">
+     <div className="max-w-screen-xl  flex items-center justify-between mx-auto p-4">
+      <div className=" text-3xl font-bold italic whitespace-nowrap ">
         Cozy Quarters
-      </span>
-    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      </div>
+    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse justify-end md:w-auto w-full ">
+      <div className='flex'>
       <button
         type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-gray-900 focus:outline-none  font-medium  text-sm px-4 py-2 text-center border-gray-400"
       >
-        Get started
+        Login
       </button>
-      {/* <button
-        data-collapse-toggle="navbar-cta"
-        type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-cta"
-        aria-expanded="false"
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
-      </button> */}
+      <div className=' w-[1px] h-6 self-center bg-gray-600'></div>
       <button
-        // data-collapse-toggle="navbar-cta"
         type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="text-gray-900 focus:outline-none font-medium text-sm px-4 py-2 text-center border-gray-400"
+      >
+        Register
+      </button>
+      </div>
+      <button
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none "
         data-drawer-target="drawer-navigation"
       data-drawer-show="drawer-navigation"
       aria-controls="drawer-navigation"
@@ -67,22 +53,73 @@ const Navbar = () => {
     </div>
     <div
       className="items-center justify-between hidden w-full md:flex md:w-auto"
-    //   id="navbar-cta"
     >
-      <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
         <li>
           <a
             href="#"
-            className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+            className="block py-2 px-3 text-gray-900 rounded md:bg-transparent hover:bg-gray-100 "
             aria-current="page"
           >
             Home
           </a>
         </li>
         <li>
+        <div>
+  <button
+    id="dropdownHoverButton"
+    data-dropdown-toggle="dropdownHover"
+    data-dropdown-trigger="hover"
+    className="text-gray-900 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+    type="button"
+  >
+    Cities
+    <svg
+      className="w-2.5 h-2.5 ms-3"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 6"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="m1 1 4 4 4-4"
+      />
+    </svg>
+  </button>
+  {/* Dropdown menu */}
+  <div
+    id="dropdownHover"
+    className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+  >
+    <ul
+      className="py-2 text-sm text-gray-700 dark:text-gray-200"
+      aria-labelledby="dropdownHoverButton"
+    >
+      {cities.map((city)=>{
+        return(
+          <li>
+        <a
+          href="#"
+          className="block px-4 py-2 hover:bg-gray-100 "
+        >
+          {city}
+        </a>
+      </li>
+        )
+      })}
+    </ul>
+  </div>
+</div>
+
+        </li>
+        <li>
           <a
             href="#"
-            className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100  "
           >
             About
           </a>
@@ -90,60 +127,24 @@ const Navbar = () => {
         <li>
           <a
             href="#"
-            className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 "
           >
-            Services
+            Feedback
           </a>
         </li>
         <li>
           <a
             href="#"
-            className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 "
           >
-            Contact
+            Contact Us
           </a>
         </li>
       </ul>
     </div>
 
-    <div>
   {/* drawer init and show */}
-  <div className="text-center">
-    {/* <button
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      type="button"
-      data-drawer-target="drawer-navigation"
-      data-drawer-show="drawer-navigation"
-      aria-controls="drawer-navigation"
-    >
-      Show navigation
-    </button> */}
-    {/* <button
-        // data-collapse-toggle="navbar-cta"
-        type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        data-drawer-target="drawer-navigation"
-      data-drawer-show="drawer-navigation"
-      aria-controls="drawer-navigation"
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
-      </button> */}
-  </div>
+    <div>
   {/* drawer component */}
   <div
     id="drawer-navigation"
