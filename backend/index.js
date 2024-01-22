4c66130b5a5553a277c04485d3e1a05c703f2f81
ashishverma2831 <ashishverma510207@gmail.com>
2024-01-22 23:58:58 +0530

    Add necessary middlewares and start the server.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+
+
+// User Routers
+
+// Initialize express
+const app = express();
+const port = 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors({
+    origin:'http://localhost:5173'
+}));
+
+
+// Starting the Server
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+})
+
+
+// Routes
+app.get('/',(req,res)=>{
+    res.send('Response from the express server')
+})
