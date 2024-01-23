0e366e7acfc260359517baf905cc73cdddb8b7c1
ashishverma2831 <ashishverma510207@gmail.com>
2024-01-23 14:28:03 +0530

    Remove unique constraints from name and email fields in the user schema.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,12 +4,10 @@ const userSchema = new Schema({
     name:{
         type:String,
         required:true,
-        unique:true
     },
     email:{
         type:String,
         required:true,
-        unique:true,
     },
     password:{
         type:String,
