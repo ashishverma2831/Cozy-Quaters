92fbe6a8bdfa87bbac8c00f44ca903feddd3fac2
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-04 13:22:46 +0530

    Refactor code to improve performance and readability

---
+++ backend/models/addRoomModel.js
@@ -0,0 +1,46 @@
+const {Schema,model} = require('mongoose');
+
+const addRoomSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    number:{
+        type:String,
+        required:true,
+    },
+    accomodation:{
+        type:String,
+        required:true,
+    },
+    suitable:{
+        type:String,
+        required:true,
+    },
+    amount:{
+        type:String,
+        required:true,
+    },
+    roomname:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    }   
+});
+
+module.exports = model('AddRoom',addRoomSchema);
