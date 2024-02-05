fe763d704e83d09ee67b616ca5d8f35f81d11bd5
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-05 18:23:10 +0530

    Add room list and room cards to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Hero from '../components/Hero'
 import Slider from '../components/Slider'
 import FeatureCard from '../components/FeatureCard'
 import rooms from '../components/roomData'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -24,6 +25,39 @@ const Home = () => {
     <>
       <Navbar />
       <Hero  searchInputRef={searchInputRef} handleSearch={handleSearch} />
+
+      <section className='my-10 mx-auto max-w-screen-xl'>
+      {
+        roomList.map((room)=>{
+          return (
+            <div className='my-4' key={room}>
+                  <div className="mx-auto max-w-sm min-w-96 bg-white border border-gray-200 hover:scale-105 duration-500 hover:duration-500 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+                    <Link href="#">
+                      <img className="rounded-t-lg w-full h-[324px]" src={room.image} alt="room photo" />
+                    </Link>
+                    <div className="p-5">
+                      <Link href="#">
+                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          {room.title.toUpperCase()}
+                        </h5>
+                        <p className='font-semibold text-md mb-2'><i className="fa-solid fa-location-dot"></i> {room.location} </p>
+                      </Link>
+                      <div className='bg-gray-300'>
+                        <div className='p-2 flex border-b'><p className='w-1/2'>Accomodation for</p><p>{room.accomodationFor}</p></div>
+                        <div className='p-2 flex border-b'><p className='w-1/2'>Suitable for</p><p>{room.suitableFor}</p></div>
+                        <div className='p-2 flex '><p className='w-1/2'>Price</p><p>Rs. {room.price}</p></div>
+                      </div>
+                      <div className='flex mt-4 justify-between'>
+                        <div><i className="fa-solid fa-user"></i> {room.ownerName} </div>
+                        {/* <div><i className="fa-regular fa-calendar-days"></i> {room.uploaded} </div> */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+          )
+        })
+      }
+      </section>
       <section className='my-10  max-w-screen-xl mx-auto'>
         <div className='p-5'>
           <p className='text-center font-bold text-4xl'>Newly added Rooms</p>
