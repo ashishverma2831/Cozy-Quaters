f4ebb917792ed9cfbdf717a899c8964850e027ab
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-05 15:57:50 +0530

    Commented out bcryptjs import and password hashing code

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Model = require('../models/userModel.js')
-const bcryptjs = require('bcryptjs');
+// const bcryptjs = require('bcryptjs');
 
 router.post('/add',(req,res)=>{
     console.log(req.body);
@@ -9,7 +9,7 @@ router.post('/add',(req,res)=>{
     // const hashedPassword = bcryptjs.hashSync(password,10);
     // const hashedConfirmPassword = bcryptjs.hashSync(confirm,10);
     
-    // new Model({name,email,password:hashedPassword,confirm:hashedConfirmPassword}).save()
+    // new Model({name,email,password:hashedPassword,confirm:hashedPassword}).save()
     new Model({name,email,password,confirm}).save()
     .then((result) => {
         res.json(result)
@@ -20,7 +20,12 @@ router.post('/add',(req,res)=>{
 })
 
 router.post('/authenticate',(req,res)=>{
+    console.log(req.body);
+    // const {email,password} = req.body;
+    // const hashedPassword = bcryptjs.hashSync(password,10);
+    // console.log(hashedPassword);
     Model.findOne(req.body)
+    // Model.findOne({email,password:hashedPassword})
     .then((result) => {
         if(result)
             res.json(result);
