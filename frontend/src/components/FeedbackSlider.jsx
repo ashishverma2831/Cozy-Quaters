2ce20699c07211aaec09ae9289d6780588b8f950
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-19 03:47:53 +0530

    Add feedback retrieval and display functionality

--- frontend/src/components/FeedbackSlider.jsx
+++ frontend/src/components/FeedbackSlider.jsx
@@ -1,46 +1,57 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const FeedbackSlider = () => {
+
+    const [feedbackList, setFeedbackList] = useState([]);
+    const fetchFeedbacks = async ()=>{
+        try {
+            const res = await fetch('http://localhost:5000/feedback/all');
+            const data = await res.json();
+            setFeedbackList(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(()=>{
+        fetchFeedbacks();
+    },[])
+
     return (
-
-
 
         <>
             <section>
-                <p className="text-center font-extrabold text-3xl my-10">Rooms</p>
+                <p className="text-center font-extrabold text-3xl my-10">Feedbacks</p>
                 <div className="max-w-screen-xl mx-auto my-10 relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
-                                <th scope="col" className="px-6 py-3">Owner Name</th>
-                                <th scope="col" className="px-6 py-3">Email</th>
-                                <th scope="col" className="px-6 py-3">Phone Number</th>
-                                <th scope="col" className="px-6 py-3">Accomodation For</th>
-                                <th scope="col" className="px-6 py-3">Suitable For</th>
-                                <th scope="col" className="px-6 py-3">Amount</th>
-                                <th scope="col" className="px-6 py-3">Room Name</th>
-                                <th scope="col" className="px-6 py-3">Description</th>
-                                <th scope="col" className="px-6 py-3">Address</th>
-                                <th scope="col" className="px-6 py-3">Delete Rooms</th>
+                                <th scope="col" className="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Email
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Rating
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Message
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Delete Feedback
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                roomList.map((room) => {
+                                feedbackList.map((feedback) => {
                                     return (
                                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                            <td className="px-6 py-4">{room.name}</td>
-                                            <td className="px-6 py-4">{room.email}</td>
-                                            <td className="px-6 py-4">{room.number}</td>
-                                            <td className="px-6 py-4">{room.accomodation}</td>
-                                            <td className="px-6 py-4">{room.suitable}</td>
-                                            <td className="px-6 py-4">{room.amount}</td>
-                                            <td className="px-6 py-4">{room.roomname}</td>
-                                            <td className="px-6 py-4">{room.description}</td>
-                                            <td className="px-6 py-4">{room.address}</td>
-                                            <td className="px-6 py-4">
-                                                <Link className='mx-2' onClick={() => { deleteRoom(room._id) }}><i className="fa-solid fa-trash text-red-700"></i></Link>
-                                            </td>
+                                            <td className="px-6 py-4">{feedback.name}</td>
+                                            <td className="px-6 py-4">{feedback.email}</td>
+                                            <td className="px-6 py-4">{feedback.rating}</td>
+                                            <td className="px-6 py-4">{feedback.message}</td>
                                         </tr>
                                     )
                                 })
